e2f9d8865c2a7bfc153d0a2f233665b95e69ac43
Kevin Tong <kcr1230@hotmail.com>
2016-11-19 11:35:04 -0800

    doGet/doPost

--- src/simplestorefront/servlet/History.java
+++ src/simplestorefront/servlet/History.java
@@ -1,4 +1,21 @@
 package simplestorefront.servlet;
 
-public class History {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class History extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    doGet(request, response);
+  }
 }
+
